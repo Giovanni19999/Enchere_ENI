05e65ba4d3daf4c8e060704e7492aeef7c479564
giova <giova@DESKTOP-AKJT30K.home>
2021-01-20 10:44:14 +0100

    Update pour fin du manager

--- src/managers/ManagerConnection.java
+++ src/managers/ManagerConnection.java
@@ -3,31 +3,53 @@ package managers;
 import javax.servlet.http.HttpServletRequest;
 
 import bo.BOConnection;
+import dal.UtilisateurDAOJdbc;
 
 
 public class ManagerConnection {
 	
-	public String validationConnection(HttpServletRequest request) {
+	public String validationConnection(HttpServletRequest request) throws Exception {
 		
 		String id=request.getParameter("identifiant");
 		String mdp=request.getParameter("mdp");
 		BOConnection connection;
+		BOConnection base;
 		String pseudo = null;
-		
-		
+		UtilisateurDAOJdbc c = new UtilisateurDAOJdbc();
+		Exception eConnection = null;
 		
 		if (id.contains("@")) {
 			connection=new BOConnection();
 			connection.setEmail(id);
 			connection.setMdp(mdp);
+			
+			
+			try {
+				base=c.selectByEmail(connection.getEmail());
+				
+				if (connection.getMdp().equals(base.getMdp())) {
+					pseudo=base.getNom();
+				}else {
+					throw eConnection;
+				}
+			}catch (Exception e){
+				throw new Exception("Erreur de connection");
+			}
 		}else {
 			connection=new BOConnection(id, mdp);
-			pseudo=connection.getNom();
+			try {
+				base=c.selectByPseudo(connection.getNom());
+				
+				if (connection.getMdp().equals(base.getMdp())) {
+					pseudo=base.getNom();
+				}else {
+					throw eConnection;
+				}
+			}catch (Exception e){
+				throw new Exception("Erreur de connection");
+			}
+			
 		}
-		
-		
-		
-		
 		
 		return pseudo;
 	}
